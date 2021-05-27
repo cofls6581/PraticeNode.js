15ca76ec89928cceab7ddb7e0c4026ca58d0e251
cofls6581 <67696767+cofls6581@users.noreply.github.com>
2021-05-28 02:41:33 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,30 @@
 //Routing
 var express=require('express');
 var app=express(); //application return
+app.use(express.static('public')); ///정적파일추가 //localhost:1500/~~
+app.get('/img',function(req,res){
+  res.send('Hi icebear! <img src="/bear.jpg">')
+});
+app.get('/page',function(req,res){
+  var lis='';
+  var time=Date();
+  for(var i=0; i<10;i++){
+    lis=lis+'<li> list</li>';
+  }
+ var print=`<!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     This is html page.<br>
+     It's ${time} now.
+     <ul>${lis}</ul>
+   </body>
+ </html>`;
+  res.send(print);
+})
 app.get('/',function(req,res){ //localhost:1500
   res.send('came from home!');
 }); //get방식 .사이트 home에 접근 사용자
