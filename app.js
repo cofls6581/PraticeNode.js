463a1fa1b39c32c7d0f8d37e82088b6c380e8bb0
cofls6581 <67696767+cofls6581@users.noreply.github.com>
2021-06-01 01:23:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,29 @@
 //Routing
 var express=require('express');
 var app=express(); //application return
+var bodyParser=require('body-parser'); //post 사용위해
+app.use(bodyParser.urlencoded({extended:false}))
 app.locals.pretty = true; //jade코드 looks pretty
 app.set('views','./views'); //jade파일들 view dir에 보관
 app.set('view engine','jade');
 app.use(express.static('public')); ///정적파일추가 //localhost:1500/~~
+app.get('/form',function(req,res){
+  res.render('form');
+});
+/*app.get('/form_receiver',function(req,res){ //url에 정보 표시됨
+  var ansone=req.query.ansone;
+  var anstwo=req.query.anstwo;
+  res.send(ansone+','+anstwo);
+});*/
+app.post('/form_receiver',function(req,res){ //body-parser이용해 req가 갖고있지않던 body객체 추가
+  //url에 정보표시안됨
+  var ansone=req.body.ansone;
+  var anstwo=req.body.anstwo;
+  res.send(ansone+','+anstwo);
+})
 app.get('/idName',function(req,res){
   res.send(req.query.id+','+req.query.name); //localhost:1500/topic?id=1&name=cl
-})
+});
 /*app.get('/topic/:id',function(req,res){ //querystring
   var numbers=[
     'First',
